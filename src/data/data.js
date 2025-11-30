602a1b2ea8207d0d82e17c74c5e228b227df961e
Sanket <mailsanketagrawal@gmail.com>
2025-11-30 13:22:31 +0530

    update

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const about = {
     name: "Vasudev Agrawal",
     role: "Final year B.Tech",
-    about: "Final Year B.Tech AIML student with hands-on experience in machine learning projects and Python development.Demonstrated skills in deep learning, recommendation systems, and web development, with practical applicationthrough internship and hackathon participation. Eager to apply technical knowledge to innovative AI solutions.",
+    about: "Final Year B.Tech AIML student with hands-on experience in machine learning projects and Python development.Demonstrated skills in deep learning, recommendation systems, and web development, with practical application through internship and hackathon participation. Eager to apply technical knowledge to innovative AI solutions.",
     email: "Mailvasudevagrawal@gmail.com",
 };
 
@@ -19,9 +19,10 @@ export const skills = [
     { name: "Html", alt: "html logo", icon: "/html.svg" },
     { name: "Css", alt: "css logo", icon: "/css.svg" },
     { name: "Js", alt: "js logo", icon: "/js.svg" },
-    { name: "Tailwind", alt: "tailwind logo", icon: "/tailwind.svg" },
+    { name: "Python", alt: "Python logo", icon: "/python.svg" },
     { name: "React Js", alt: "react logo", icon: "/react.svg" },
     { name: "Next Js", alt: "nextjs logo", icon: "/nextjs.svg" },
+    { name: "java", alt: "java logo", icon: "/java.svg" },
 ];
 
 export const socials = {
@@ -34,17 +35,17 @@ export const socials = {
 export const projects = {
     pinProjects: [
         {
-            title: "Project 1",
+            title: "Fashion Recommendation System",
             description:
-                "A sleek, modern portfolio site to showcase your work and skills. Responsive design with smooth navigation and SEO optimization.",
-            tags: ["CSS", "React Js"],
+                " Developed an image-based clothing recommender using pre-trained ResNet50 for feature extraction and Nearest Neighbors algorithm for matching visually similar fashion items Implemented both collaborative and content-based filtering techniques to enhance recommendation accuracy Built an interactive web interface using Streamlit for intuitive user experience and result visualization ",
+            tags: ["Python","Deep Learning","Streamlit"],
             link: "",
         },
         {
-            title: "Project 2",
+            title: " Sentiment-Aware Product Recommendation System",
             description:
-                "A clean, minimal blog setup featuring dynamic content, category filters, and easy-to-read layouts. Perfect for sharing your ideas.",
-            tags: ["CSS", "React Js"],
+                "Designed and implemented a Sentiment-Aware Product Recommendation System integrating sentiment analysis with collaborative filtering.Prioritized recommendations based on sentiment-weighted product similarity.Built using Python, scikit-learn/TensorFlow, and optional Flask web interface.Improved recommendation relevance by filtering out low-sentiment products.A clean, minimal blog setup featuring dynamic content, category filters, and easy-to-read layouts. Perfect for sharing your ideas.",
+            tags: ["Docker","AWS", "MYSQL","Mongodb"],
             link: "",
         },
         {
@@ -82,10 +83,10 @@ export const projects = {
 
 export const experience = [
     {
-        title: "Frontend Developer at Tech Solutions",
+        title: "Software Developer Intern at Technotribes",
         description:
-            "Worked on building and maintaining responsive web applications using React.js and Next.js, improving UI performance and user experience.",
-        date: "15/01/2024",
+            "Completed multiple projects using AI/ML with strong problem-solving and implementation skills.Demonstrated solid technical abilities and high-quality documentation throughout all tasks.Shown commendable dedication, consistency, and responsibility in every assigned role.Contributed effectively as a valuable member of the team.Wishing continued success in all future endeavours.Worked on building and maintaining responsive web applications using React.js and Next.js, improving UI performance and user experience.",
+        date: "03/03/2025",
     },
     {
         title: "Web Developer Intern at Creative Studio",
@@ -103,7 +104,7 @@ export const experience = [
 
 export const blogs = [
     {
-        title: "Understanding React Hooks",
+        title: "How I Built My First End-to-End Machine Learning Project",
         date: "12/03/2025",
         link: "#",
     },
